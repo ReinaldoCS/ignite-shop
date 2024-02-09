130efe6aba42b5990c7523aaf71ea79e0b67d35e
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-02-09 07:55:10 -0300

    Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
+
+import logoImg from '../assets/logo.svg'
 
 globalStyles() // Importa estilos globais sem renderizar novamente quando a página mudar
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Logotipo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
