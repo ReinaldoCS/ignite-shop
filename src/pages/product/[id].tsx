7a839174f8bd5a1a0309dc115aca448f0d9e4fe0
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-02-15 07:46:28 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,31 @@
 import { useRouter } from 'next/router'
 
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
+
 export default function Product() {
   const { query } = useRouter()
-  return <h1>Produtos: {query.id}</h1>
+
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 100,00</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          eius maiores voluptatem cumque eveniet cupiditate nobis natus debitis
+          doloribus ex ratione dolorum voluptatum sint eaque nihil, enim non est
+          minima.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
